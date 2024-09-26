bbcc8f5a1ca5b763dcb6400723b404397121667b
RubenErhardt <rubengamebattles@outlook.com>
2024-09-26 17:36:11 +0200

    Iets meer toegankelijk

--- src/lib/artikel-klein.svelte
+++ src/lib/artikel-klein.svelte
@@ -5,11 +5,13 @@
 
 {#if data.posts}
     {#each data.posts.slice(0, numberOfPosts) as post}
-    <article>
-        <img src={post.yoast_head_json.og_image[0].url} alt="Artikel afbeelding">
+    <a href="/" class="article-link" aria-label="">
+        <article>
+            <img src={post.yoast_head_json.og_image[0].url} alt="Artikel afbeelding">
             <h3>{@html post.title.rendered}</h3>
             <p>door <strong>{@html post.yoast_head_json.author}</strong></p>
         </article>
+    </a>
     {/each}
     
 {:else}
@@ -17,11 +19,16 @@
 {/if}
 
 <style>
+    .article-link {
+        text-decoration: none; 
+    }
+
     article {
         max-width: 260px;
         margin: 10px;
         height: 350px;
         border: 1.6px solid #7B7B7A;
+        cursor: pointer;
     }
 
     article img {
@@ -34,6 +41,7 @@
         padding: 10px;
         margin-bottom: 50px;
         margin-top: 0;
+        color: black;
     }
 
     article p {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,21 +11,5 @@
     
 </script>
 
-{#if data.posts}
-    {#each data.posts as post}
-    <!-- @html means: there is html in this string, render it -->
-        <a href={post.slug}>
-            <h3>{@html post.title.rendered}</h3>
-        </a>
-        <p>{@html post.excerpt.rendered}</p>
-        <img src={post.yoast_head_json.og_image[0].url} alt="Artikel afbeelding">
-        <p>{(new Date(post.date)).toLocaleDateString("nl-NL", dateFormat)}</p>
-        <p>{post.yoast_head_json.twitter_misc["Geschatte leestijd"]}</p>
-        <p>{post.yoast_head_json.author}</p>
-    {/each}
-{:else}
-    <!-- This will show if no posts are available -->
-    <p>No posts available</p>
-{/if}
 
 <ArtikelKlein data={data}/>
