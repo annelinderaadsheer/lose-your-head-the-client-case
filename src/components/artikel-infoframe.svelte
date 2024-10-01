8787889cfc74a7461c6fe98e3c509e50d42160f6
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2024-10-01 16:48:33 +0200

    added background ellipse

--- src/components/artikel-infoframe.svelte
+++ src/components/artikel-infoframe.svelte
@@ -10,24 +10,38 @@
 
 <article>
     <div class="summary">
-        <p class="catagory">{post.yoast_head_json.schema["@graph"][0].articleSection[0]}</p>
+        <p class="catagory bold">{post.yoast_head_json.schema["@graph"][0].articleSection[0]}</p>
         <h1>{@html post.title.rendered}</h1>
-        <p>{@html post.excerpt.rendered}</p>
+        {@html post.excerpt.rendered}
         <div class="author-and-read-time">
             <p>Door <span class="bold">{post.authors[0].display_name}</span></p>
             <p>{post.yoast_head_json.twitter_misc["Geschatte leestijd"]}</p>
         </div>
     </div>
     <div class="img-container">
-        <p>{(new Date(post.date)).toLocaleDateString("nl-NL", dateFormat)}</p>
         <img src={post.yoast_head_json.og_image[0].url} alt="Artikel afbeelding">
+        <p>{(new Date(post.date)).toLocaleDateString("nl-NL", dateFormat)}</p>
     </div>
 </article>
 
 <style>
+    :global(body) {
+        background-color: white;
+
+        background-image: url('/ellipse.svg');
+        background-repeat: no-repeat;
+        background-size: 5000px 760px;
+        background-position: -2000px -380px;
+    }
+
+    * {
+        text-align: center;
+    }
+
     article {
         display: flex;
-        gap: 3em;
+        flex-direction: column;
+        align-items: center;
         width: 100%;
     }
 
@@ -35,16 +49,7 @@
         display: flex;
         flex-direction: column;
         justify-content: center;
-        width: 45%;
-    }
-
-    .img-container {
-        width: 55%;
-    }
-
-    .author-and-read-time {
-        display: flex;
-        justify-content: space-between;
+        padding: 2em 0;
     }
 
     img {
@@ -55,5 +60,37 @@
 
     .bold {
         font-weight: bold;
+    }
+
+    .catagory {
+        color: var(--accent-color1);
+        text-transform: uppercase;
+    }
+
+    @media only screen and (min-width: 960px) {
+        article {
+            flex-direction: row;
+            gap: 2em;
+        }
+
+        * {
+            text-align: start;
+        }
+
+        .img-container {
+            display: flex;
+            flex-direction: column-reverse;
+            align-items: end;
+            width: 55%;
+        }
+
+        .summary {
+            width: 45%;
+        }
+
+        .author-and-read-time {
+            display: flex;
+            justify-content: space-between;
+        }
     }
 </style>
