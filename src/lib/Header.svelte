1ad592149c4398cc11180417aef56dd5edf76e96
RubenErhardt <rubengamebattles@outlook.com>
2024-10-03 13:50:33 +0200

    data

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,7 @@
+<script>
+    export let data:
+</script>
+
 <header>
 <section class="boven">
     <ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script>
     import Header from '../lib/Header.svelte';
+      /** @type {import('./$types').PageData} */
+      export let data;
+
+const dateFormat = {
+    month: 'short',
+    day: 'numeric',
+};
     
 </script>
 
-<Header />
+<Header data={data}/>
 
