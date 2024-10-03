1a2473c9926102ad639084bbbdb098d0f4d1b6e3
RubenErhardt <rubengamebattles@outlook.com>
2024-10-03 13:33:20 +0200

    Begin Header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,42 @@
+<header>
+<section class="boven">
+    <ul>
+        <li>Colofon</li>
+        <li>Over</li>
+        <li>Meedoen</li>
+        <li>Contact</li>
+    </ul>
+</section>
+<section class="midden">
+    <div class="datum"></div>
+    <img src="" alt="">
+    <ul>
+        <li>Nieuwsbrief</li>
+        <li><button></button></li>
+        <li><img src="" alt=""></li>
+    </ul>
+    
+</section>
+<section class="onder"></section>
+</header>
+
+<style>
+    .boven{
+        background-color: black;
+        display: flex;
+        color: white;
+        justify-content: end;
+    }
+
+    .boven ul {
+    display: flex; /* Flexbox toepassen op de ul om items naast elkaar te plaatsen */
+    list-style: none; /* Verwijdert de standaard bullet points */
+    margin: 0;
+    cursor: pointer;
+    padding: 0;
+}
+
+.boven ul li {
+    padding: 15px;
+}
+</style>
