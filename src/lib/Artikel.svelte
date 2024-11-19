e0059f9526db3dfebe703b172657164530bd6d27
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2024-11-20 00:55:27 +0100

    artikel component basis af

---
+++ src/lib/Artikel.svelte
@@ -0,0 +1,57 @@
+<script>
+    export let post
+
+    const dateFormat = {
+        month: 'short',
+        day: 'numeric',
+    };
+</script>
+
+<a href="/{post.slug}">
+    <article>
+        <img src={post.yoast_head_json.og_image[0].url} alt="Artikel afbeelding" width="25em" height="15em">
+        <div class="title">
+            <h1 class="artikel-kop">{@html post.title.rendered}</h1>
+        </div>
+        <div class="info-text">
+            <p>{post.yoast_head_json.author}</p>
+            <p>{post.yoast_head_json.twitter_misc["Geschatte leestijd"]}</p>
+            <p>{(new Date(post.date)).toLocaleDateString("nl-NL", dateFormat)}</p>
+        </div>
+    </article>
+</a>
+
+<style>
+    article {
+        border: var(--border);
+        width: 25em;
+        height: 30em;
+    }
+
+    img {
+        width: 25em;
+        height: 15em;
+        border-bottom: var(--border);
+    }
+
+    h1 {
+        font-family: var(--font-alt);
+        font-weight: var(--font-style-bold);
+        font-size: 28px;
+        line-height: 32px;
+        letter-spacing: 0.25px;
+    }
+
+    .title {
+        width: 25em;
+        height: 11em;
+        padding: 0.5em;
+        overflow: hidden;
+    }
+
+    .info-text {
+        display: flex;
+        height: 2em;
+        border-top: var(--border);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
     
     import Footer from '$lib/Footer.svelte';
     import Header from '$lib/Header.svelte';
+    import Artikel from '$lib/Artikel.svelte';
 </script>
 
 <Header/>
@@ -17,14 +18,7 @@
     {#if data.posts}
         {#each data.posts as post}
             <!-- @html means: there is html in this string, render it -->
-            <a href="/{post.slug}">
-                <h3>{@html post.title.rendered}</h3>
-            </a>
-            <p>{@html post.excerpt.rendered}</p>
-            <img src={post.yoast_head_json.og_image[0].url} alt="Artikel afbeelding">
-            <p>{(new Date(post.date)).toLocaleDateString("nl-NL", dateFormat)}</p>
-            <p>{post.yoast_head_json.twitter_misc["Geschatte leestijd"]}</p>
-            <p>{post.yoast_head_json.author}</p>
+            <Artikel post={post} />
         {/each}
     {:else}
         <!-- This will show if no posts are available -->
@@ -35,8 +29,11 @@
 <Footer />
 
 <style>
-    img {
-        max-width: 100%;
-        height: auto;
+    main {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1em;
+        margin-top: 1em;
+        justify-content: center;
     }
 </style>
