8e364de9c11a3cdfdf37b5f0cc7e10f5379b8736
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2024-11-28 00:22:47 +0100

    category preview done, added lazy loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     /** @type {import('./$types').PageData} */
     export let data;
-
+    
     const dateFormat = {
         month: 'short',
         day: 'numeric',
@@ -10,31 +10,41 @@
     import Footer from '$lib/Organism/Footer.svelte';
     import Header from '$lib/Organism/Header.svelte';
     import Artikel from '$lib/Organism/Artikel.svelte';
+    import CategoryPreview from '$lib/Organism/CategoryPreview.svelte';
 
 </script>
 
-<Header/>
+<!-- <Header/> -->
 
-<main>
-    {#if data.posts}
-        {#each data.posts as post}
-            <!-- @html means: there is html in this string, render it -->
-            <Artikel post={post} />
+<div class="background">
+    <main>
+        {#if data.posts}
+            {#each data.posts as post}
+                <!-- @html means: there is html in this string, render it -->
+                <Artikel post={post} />
+            {/each}
+        {:else}
+            <!-- This will show if no posts are available -->
+            <p>No posts available</p>
+        {/if}
+    
+        {#each data.categories as category}
+            <CategoryPreview categoryName={category.name} categorySlug={category.slug} posts={category.posts} />
         {/each}
-    {:else}
-        <!-- This will show if no posts are available -->
-        <p>No posts available</p>
-    {/if}
-</main>
+    </main>
+</div>
 
-<Footer />
+<!-- <Footer /> -->
 
 <style>
+    .background {
+        background-color: #f5f5f5;
+    }
     main {
         display: flex;
         flex-wrap: wrap;
         gap: 1em;
-        margin-top: 1em;
+        padding-top: 1em;
         justify-content: center;
     }
 </style>
